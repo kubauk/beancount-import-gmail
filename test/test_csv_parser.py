bf9024c97f50515a2cff85d297c1fc1c011a9f8c
Jakub Jamro <kuba.jamro@gmail.com>
2020-12-29 10:50:22 +0000

    tidying up the test structure

--- test/test_csv_parser.py
+++ test/test_csv_parser.py
@@ -9,5 +9,5 @@ def file_memo(file_name):
 
 def test_transactions_match_csv():
 
-    memo = csv_parser.extract_paypal_transactions_from_csv_memo(file_memo("test.csv"))
+    memo = csv_parser.extract_paypal_transactions_from_csv(file_memo("test.csv"))
     pass
