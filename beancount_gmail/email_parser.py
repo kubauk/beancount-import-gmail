f53a4356ad9d033fd98f8d2530b32256d65f4733
Jakub Jamro <kuba.jamro@gmail.com>
2021-04-08 17:14:38 +0100

    adding debugging to write out data to file for review

--- beancount_gmail/email_parser.py
+++ beancount_gmail/email_parser.py
@@ -3,6 +3,7 @@ import os
 from mailbox import Message
 
 import bs4
+import jsonpickle
 import pytz
 
 from beancount_gmail.uk_paypal_email.parser import find_receipts
@@ -11,7 +12,11 @@ CUT_OFF_DATE = datetime.datetime(2009, 1, 1, tzinfo=pytz.utc)
 
 EXCLUDED_DATA_DIR = "./excluded"
 
+DEBUGGING_DATA_DIR = "./debugging"
+
 TIMEZONE = pytz.timezone("Europe/London")
+
+WRITE_DEBUG = False
 
 
 class NoCharsetException(Exception):
@@ -35,10 +40,10 @@ def get_charset(message):
 
 def process_message_text(message_date, message):
     if message.get_content_type() == "text/html":
-        return write_debugging_file_on_exception(extract_receipts_from_email, "html", message_date,
-                                                 message.get_payload(decode=True).decode(get_charset(message)))
+        return maybe_write_debugging(extract_receipts_from_email, "html", message_date,
+                                     message.get_payload(decode=True).decode(get_charset(message)))
     elif message.get_content_type == "text/plain:":
-        return write_debugging_file_on_exception(extract_receipts_from_email, "txt", message_date, message)
+        return maybe_write_debugging(extract_receipts_from_email, "txt", message_date, message)
     else:
         return list()
 
@@ -54,16 +59,31 @@ def process_message_payload(message_date, message):
         return process_message_text(message_date, message)
 
 
-def write_debugging_data_to_file(reason, extension, message_date, message):
-    if not os.path.exists(EXCLUDED_DATA_DIR):
-        os.mkdir(EXCLUDED_DATA_DIR)
-
-    file = os.path.join(EXCLUDED_DATA_DIR, "%s.%s.%s" % (message_date, reason, extension))
-    with open(file, "w") as out:
+def write_email_to_file(reason, extension, message_date, message, file_dir):
+    def data_handler(out):
         if isinstance(message, Message):
             out.write(message.as_string())
         else:
             out.write(message)
+
+    write_data_to_file(reason, extension, message_date, data_handler, file_dir)
+
+
+def write_receipts_to_file(receipts, message_date):
+    def data_handler(out):
+        encode = jsonpickle.encode(receipts, unpicklable=True, indent=2)
+        out.write(encode)
+
+    write_data_to_file(None, "json", message_date, data_handler, DEBUGGING_DATA_DIR)
+
+
+def write_data_to_file(reason, extension, message_date, data_handler, file_dir):
+    if not os.path.exists(file_dir):
+        os.mkdir(file_dir)
+
+    file = os.path.join(file_dir, "%s.%s.%s" % (message_date, reason, extension))
+    with open(file, "w") as out:
+        data_handler(out)
 
 
 def extract_receipts(message):
@@ -71,7 +91,7 @@ def extract_receipts(message):
     message_date = TIMEZONE.normalize(local_message_date.astimezone(TIMEZONE))
 
     if message_date < CUT_OFF_DATE:
-        write_debugging_data_to_file("TooOld", "eml", message_date, message)
+        write_email_to_file("TooOld", "eml", message_date, message, EXCLUDED_DATA_DIR)
         return list()
 
     try:
@@ -80,9 +100,21 @@ def extract_receipts(message):
         return list()
 
 
-def write_debugging_file_on_exception(fn, extension, message_date, message):
+def maybe_write_debugging(fn, extension, message_date, message):
+    if WRITE_DEBUG:
+        write_email_to_file(None, extension, message_date, message, DEBUGGING_DATA_DIR)
+
+    receipts = write_email_file_on_exception(fn, extension, message_date, message)
+
+    if WRITE_DEBUG:
+        write_receipts_to_file(receipts, message_date)
+
+    return receipts
+
+
+def write_email_file_on_exception(fn, extension, message_date, message):
     try:
         return fn(message_date, message)
     except Exception as e:
-        write_debugging_data_to_file(e.__class__.__name__, extension, message_date, message)
+        write_email_to_file(e.__class__.__name__, extension, message_date, message, EXCLUDED_DATA_DIR)
         raise e
