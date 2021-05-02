5f378575a712073e72897a12b3f2dce9bb1f8c4a
Jakub Jamro <kuba.jamro@gmail.com>
2021-05-02 13:59:15 +0100

    Handle email processing edge cases
    If we do not have a known content type like text/html or text/plain then we fail gracefully and the parser is not invoked.
    
    Issue #10

--- test/test_email_processing.py
+++ test/test_email_processing.py
@@ -32,3 +32,34 @@ def test_text_is_given_to_parse_in_absence_of_html(email_message):
     parser.extract_receipts.assert_called_once()
     assert_that(receipts, is_(RECEIPTS))
     pass
+
+
+def test_when_parsing_fails_no_receipts_are_returned(email_message):
+    message = email_message("canned_messages/html.eml")
+    parser = Mock(spec=EmailParser)
+    parser.extract_receipts.side_effect = Exception("test")
+
+    receipts = extract_receipts(parser, message)
+
+    parser.extract_receipts.assert_called_once()
+    assert_that(receipts, is_([]))
+
+
+def test_multipart_message_with_no_text_or_html_content_type_returns_no_receipts(email_message):
+    message = email_message('canned_messages/unknown-content-type-multipart.eml')
+    parser = Mock(spec=EmailParser)
+
+    receipts = extract_receipts(parser, message)
+
+    parser.extract_receipts.assert_not_called()
+    assert_that(receipts, is_([]))
+
+
+def test_message_with_no_text_or_html_content_type_returns_no_receipts(email_message):
+    message = email_message('canned_messages/unknown-content-type.eml')
+    parser = Mock(spec=EmailParser)
+
+    receipts = extract_receipts(parser, message)
+
+    parser.extract_receipts.assert_not_called()
+    assert_that(receipts, is_([]))
