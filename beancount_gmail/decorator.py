067ba312779bd8f2b3ac3fea71b1fb74405a1dff
Jakub Jamro <kuba.jamro@gmail.com>
2021-08-15 14:07:00 +0100

    Add decorator configuration and make it generic to all parsers.
    Decorator will work for all parsers.
    
    Issue #3 and #4

---
+++ beancount_gmail/decorator.py
@@ -0,0 +1,27 @@
+from functools import wraps
+
+import gmails
+from beancount.core.data import Transaction
+
+from beancount_gmail.importer import download_and_match_transactions
+
+
+def _interesting_transaction(transaction: Transaction):
+    return isinstance(transaction, Transaction) and 'Luxembourg, eBay' in transaction.narration
+
+
+def _add_email_details(parser, email_address, credentials_directory, postage_account, transactions):
+    retriever = gmails.retriever.Retriever('beancount-import-gmail', email_address, credentials_directory)
+    return download_and_match_transactions(parser, retriever,
+                                           list(filter(_interesting_transaction, transactions)), postage_account)
+
+
+def gmail_import(parser, email_address, credentials_directory, postage_account):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(self, *args, **kwargs):
+            transactions = func(self, *args, **kwargs)
+            _add_email_details(parser, email_address, credentials_directory, postage_account, transactions)
+            return transactions
+        return wrapper
+    return decorator
