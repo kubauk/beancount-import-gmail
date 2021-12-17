d5f08f4bd66792d08f13614f48486846ff42bd31
Jakub Jamro <kuba.jamro@gmail.com>
2021-12-17 23:32:02 +0000

    Adding another amazon email test

--- test/uk_amazon_email/test_parsing_of_emails.py
+++ test/uk_amazon_email/test_parsing_of_emails.py
@@ -18,3 +18,14 @@ def test_parse_amazon_order_2021_11_24(soup):
                                    currency="GBP")
 
 
+def test_parse_amazon_order_2021_11_30(soup):
+    beautiful_soup = soup("sample_html/amazon-order-2021-11-30.html")
+    receipts = extract_receipts(datetime.now(), beautiful_soup)
+
+    assert_receipt_with_one_detail(receipts[0],
+                                   "8.99",
+                                   "Gukasxi Tabletop Christmas Tree,Mini Artificial Christmas Pine Trees with Plastic "\
+                                   "Base Season Ornaments Tabletop Trees for Xmas Holiday Party Home Decor,with 2M "\
+                                   "String Lights and Ornaments Condition: NewSold by:Feng_x Fulfilled by Amazon",
+                                   "8.99",
+                                   currency="GBP")
