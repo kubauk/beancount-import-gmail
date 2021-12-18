b66890fc7f95606ace783a35586e4a45663cbc25
Jakub Jamro <kuba.jamro@gmail.com>
2021-12-18 01:25:13 +0000

    Parser calls parsing method

--- beancount_gmail/uk_amazon_email/__init__.py
+++ beancount_gmail/uk_amazon_email/__init__.py
@@ -4,11 +4,12 @@ from bs4 import BeautifulSoup
 
 from beancount_gmail.email_parser_protocol import EmailParser
 from beancount_gmail.receipt import Receipt
+from beancount_gmail.uk_amazon_email.parsing import extract_receipts
 
 
 class UKAmazonParser(EmailParser):
     def extract_receipts(self, message_date: datetime, soup: BeautifulSoup) -> list[Receipt]:
-        raise Exception
+        return extract_receipts(message_date, soup)
 
     def search_query(self) -> str:
         return r'"Your Amazon.co.uk order" auto-confirm@amazon.co.uk'
