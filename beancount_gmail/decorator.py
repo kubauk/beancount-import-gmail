17f3968eeeb119d2b17fe92fb05fa5e4cdf2694e
Jakub Jamro <kuba.jamro@gmail.com>
2021-12-18 01:25:42 +0000

    Decorator has a retriever cache to prevent need for multiple logins

--- beancount_gmail/decorator.py
+++ beancount_gmail/decorator.py
@@ -1,17 +1,25 @@
 from datetime import timedelta
-
-from beancount.core.data import Transaction
 from functools import wraps
 
 import gmails
+from beancount.core.data import Transaction
 
 from beancount_gmail.downloading_and_matching import download_and_match_transactions
 from beancount_gmail.email_parser_protocol import EmailParser
 
+_RETRIEVER_CACHE = dict()
 
-def _add_email_details(parser: EmailParser, email_address: str, credentials_directory: str, postage_account: str,
-                       transactions: list[Transaction], search_delta: timedelta = timedelta()) -> None:
-    retriever = gmails.retriever.Retriever('beancount-import-gmail', email_address, credentials_directory)
+
+def _add_email_details(parser: EmailParser,
+                       email_address: str,
+                       credentials_directory: str,
+                       postage_account: str,
+                       transactions: list[Transaction],
+                       search_delta: timedelta = timedelta()) -> None:
+    retriever = _RETRIEVER_CACHE.setdefault((email_address, credentials_directory),
+                                            gmails.retriever.Retriever('beancount-import-gmail',
+                                                                       email_address,
+                                                                       credentials_directory))
     download_and_match_transactions(parser, retriever, transactions, postage_account, search_delta)
 
 
