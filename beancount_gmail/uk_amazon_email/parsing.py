284c65c956feb316fb16118f776591674004200d
Jakub Jamro <kuba.jamro@gmail.com>
2021-12-18 10:35:06 +0000

    Don't parse descriptions of suggested products

--- beancount_gmail/uk_amazon_email/parsing.py
+++ beancount_gmail/uk_amazon_email/parsing.py
@@ -6,13 +6,27 @@ from beancount_gmail.common.parsing import extract_row_text
 from beancount_gmail.receipt import Receipt, TOTAL
 
 
+def index_containing(table_text, needle):
+    for i, row in enumerate(table_text):
+        if needle in row:
+            return i
+    return -1
+
+
+def truncate_end_with_product_suggestions(table_text: list[list[str]]) -> list[list[str]]:
+    end = index_containing(table_text, 'Customers Who Bought Items in Your Order Also Bought')
+    return table_text[0:end] if end > 0 else table_text
+
+
 def extract_receipts(message_date: datetime, beautiful_soup: bs4.BeautifulSoup) -> list[Receipt]:
+    receipts = list()
+
     table_text = [extract_row_text(table) for table in beautiful_soup.find_all('table')
                   if table.find('table') is None]
 
-    receipts = list()
+    order_only = truncate_end_with_product_suggestions(table_text)
 
-    interesting = (row for row in table_text if len([entry for entry in row if '£' in entry]) > 0)
+    interesting = (row for row in order_only if len([entry for entry in row if '£' in entry]) > 0)
     for t, d in zip(interesting, interesting):
         receipts.append(Receipt(message_date,
                                 [(d[1], "{} GBP".format(d[2]))],
--- test/uk_amazon_email/test_parsing_of_emails.py
+++ test/uk_amazon_email/test_parsing_of_emails.py
@@ -5,6 +5,13 @@ from beancount_gmail.uk_amazon_email.parsing import extract_receipts
 from test.test_receipt import assert_receipt_with_one_detail, assert_receipt_with_details
 
 
+def test_parse_amazon_order_2021_03_11_no_order_details(soup):
+    beautiful_soup = soup("sample_html/amazon-order-2021-03-11-no-order-details.html")
+    receipts = extract_receipts(datetime.now(), beautiful_soup)
+
+    assert_that(len(receipts), is_(0))
+
+
 def test_parse_amazon_order_2021_11_24(soup):
     beautiful_soup = soup("sample_html/amazon-order-2021-11-24.html")
     receipts = extract_receipts(datetime.now(), beautiful_soup)
@@ -13,8 +20,8 @@ def test_parse_amazon_order_2021_11_24(soup):
                                    "28.98",
                                    "Moongiantgo Coffee Grinder Manual Compact & Effortless, Stainless Steel & Glass, " \
                                    "Adjustable Coarseness Ceramic Burr - 5 Precise Levels, with Spoon & Brush | Make " \
-                                   "Fresh Coffee Anytime & Anywhere Condition: NewSold by:Moongiantgo-Eu Fulfilled by " \
-                                   "Amazon",
+                                   "Fresh Coffee Anytime & Anywhere Condition: NewSold by:Moongiantgo-Eu Fulfilled " \
+                                   "by Amazon",
                                    "31.50")
 
 
@@ -43,8 +50,8 @@ def test_parse_amazon_order_2021_11_30(soup):
 
     assert_receipt_with_one_detail(receipts[0],
                                    "8.99",
-                                   "Gukasxi Tabletop Christmas Tree,Mini Artificial Christmas Pine Trees with Plastic " \
-                                   "Base Season Ornaments Tabletop Trees for Xmas Holiday Party Home Decor,with 2M " \
-                                   "String Lights and Ornaments Condition: NewSold by:Feng_x Fulfilled by Amazon",
-                                   "8.99",
-                                   currency="GBP")
+                                   "Gukasxi Tabletop Christmas Tree,Mini Artificial Christmas Pine Trees with " \
+                                   "Plastic Base Season Ornaments Tabletop Trees for Xmas Holiday Party Home " \
+                                   "Decor,with 2M String Lights and Ornaments Condition: NewSold by:Feng_x Fulfilled " \
+                                   "by Amazon",
+                                   "8.99")
